a5c282f4c3eee5feb35632e5bfce50df5b41a532
OJortiz <OJORTIZP@correo.url.edu.gt>
2026-02-09 18:58:34 -0600

    Reto 1
    Código eliminado y puesto en test/procesar.js

--- api/procesar.js
+++ api/procesar.js
@@ -6,24 +6,3 @@ export default function handler(req, res) {
   });
 }
 
-test("procesar maneja nombre ausente", () => {
-  const req = { query: {} };
-
-  const res = {
-    statusCode: null,
-    body: null,
-    status(code) {
-      this.statusCode = code;
-      return this;
-    },
-    json(payload) {
-      this.body = payload;
-      return this;
-    }
-  };
-
-  handler(req, res);
-
-  assert.equal(res.statusCode, 200);
-  assert.ok(res.body.resultado.includes("ANÓNIMO"));
-});
